a1977d63afea0515375200ed34e1ba524ed23877
sushil <baymax732001@gmail.com>
2023-07-11 22:14:51 +0530

    logo

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/global_variable.dart';
+import 'package:instagram_clone/widgets/post_card.dart';
+
 class FeedScreen extends StatelessWidget {
   const FeedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Feed Screen"),);
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          centerTitle: false,
+          title: SvgPicture.asset(
+            instagramIcon,
+            height: 30,
+            color: primaryColor,
+          ),
+          actions: const [
+            IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.messenger_outline,
+                  color: primaryColor,
+                ))
+          ],
+        ),
+        body: const PostCard());
   }
 }

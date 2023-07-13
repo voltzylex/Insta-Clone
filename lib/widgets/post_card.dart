583d9f8c4d8ea63e00f5c18f3ea76a1cfdc32e3a
sushil <baymax732001@gmail.com>
2023-07-13 22:44:59 +0530

    post ui

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/global_variable.dart';
 
 class PostCard extends StatelessWidget {
   const PostCard({super.key});
@@ -7,8 +8,108 @@ class PostCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 10),
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
       decoration: const BoxDecoration(color: mobileBackgroundColor),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 0)
+                .copyWith(right: 0),
+            child: Row(
+              children: [
+                // HEADER Section
+                const CircleAvatar(
+                  radius: 26,
+                  backgroundImage: NetworkImage(avatarImage),
+                ),
+                const Expanded(
+                  child: Padding(
+                      padding: EdgeInsets.only(left: 8),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Name",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      )),
+                ),
+                IconButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) => Dialog(
+                              child: ListView(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 16),
+                                shrinkWrap: true,
+                                children: ["Delete", "Edit"]
+                                    .map((e) => InkWell(
+                                          onTap: () {},
+                                          child: Container(
+                                            padding: const EdgeInsets.symmetric(
+                                                vertical: 12, horizontal: 16),
+                                            child: Text(e),
+                                          ),
+                                        ))
+                                    .toList(),
+                              ),
+                            ));
+                  },
+                  icon: const Icon(
+                    Icons.more_vert_outlined,
+                    color: primaryColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // IMAGE SECTION
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
+            width: double.infinity,
+            child: Image.network(
+              avatarImage,
+              fit: BoxFit.cover,
+            ),
+          ),
+          // LIKE COMMENT SECTION
+          const Row(
+            children: [
+              IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ),
+              IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.comment,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.send,
+                  color: Colors.white,
+                ),
+              ),
+              Expanded(child: SizedBox()),
+              IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.bookmark_border,
+                    color: Colors.white,
+                  ))
+            ],
+          )
+        ],
+      ),
     );
   }
 }
